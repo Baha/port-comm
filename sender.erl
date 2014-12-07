a061378cfa315df2661811f8339268230dc3ae39
Baha <ukonat@gmail.com>
2014-12-08 00:36:16 +0100

    Erl process prints tracing messages.

--- sender.erl
+++ sender.erl
@@ -3,11 +3,19 @@
 
 start() ->
   Cmd = "python -u receiver.py",
+  Numlist = "1,2,3\n",
+
+  % Open port
   Port = open_port({spawn, Cmd},[]),
-  port_command(Port, "1,2,3\n"),
+  io:format("Port opened at ~p.~n",[Port]),
+  % Send data
+  port_command(Port, Numlist),
+  io:format("Sent number list ~p through the port.~n", [Numlist]),
+
+  % Wait for data
   receive
     {Port, {data, Data}} ->
-      io:format("~p~n", [Data]);
+      io:format("Received sum of numbers through the port: ~p.~n", [Data]);
     _ ->
       io:format("Received data from Python process with bad format.~n")
   end.
