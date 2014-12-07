19bd2ec87642b4c94d3bca09ca79c783eec44db9
Baha <ukonat@gmail.com>
2014-12-08 00:21:48 +0100

    Python adds numbers received from the port.

--- receiver.py
+++ receiver.py
@@ -1,3 +1,6 @@
 import sys
 
-sys.stdout.write("Python process spawned!")
+if __name__ == "__main__":
+  port_data = sys.stdin.readline()
+  num_list = [int(num) for num in port_data.strip().split(',')]
+  sys.stdout.write(str(sum(num_list)))
--- sender.erl
+++ sender.erl
@@ -4,6 +4,7 @@
 start() ->
   Cmd = "python -u receiver.py",
   Port = open_port({spawn, Cmd},[]),
+  port_command(Port, "1,2,3\n"),
   receive
     {Port, {data, Data}} ->
       io:format("~p~n", [Data]);
